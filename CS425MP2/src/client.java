2d0daab93df6adbf7e9edfb96dcff6739fd81b53
liangyc <liang51@illinois.edu>
2012-11-10 20:43:09 -0600

    push client

---
+++ CS425MP2/src/client.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import failureDetection_Membership.HeartbeatSender;
+
+
+public class client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//args[0] is the ip address connection to 
+		public DatagramSocket sock;
+		
+		Scanner s = new Scanner(System.in);
+		String cmd = null;
+		while ((cmd = s.nextLine()) != null) {
+
+			if ("exit".equals(cmd)) {
+				System.exit(0);
+			}
+			else if(cmd.startsWith("put ")){
+				sendMsg(sock, args[0], cmd, Machine.FILE_TRANSFER_PORT);
+				String sourceFN = cmd.substring(4, cmd.indexOf(' ', 4));
+				
+				Runnable runnable = new FileTransferServer(sourceFN);
+				Thread thread = new Thread(runnable);
+				thread.start();
+				
+			}
+			
+		}
+
+	}
+	
+	public static void sendMsg(DatagramSocket sock, String ip, String msg, int portN) {
+		try {
+			InetAddress ipAddr = InetAddress.getByName(ip);
+			byte[] sendData = msg.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData,
+					sendData.length, ipAddr, portN);
+			sock.send(sendPacket);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
